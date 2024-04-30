e757747b6c9aaa8fbc11a5f44e190cb1fe61a220
Zetsubowww <gianneola@gmail.com>
2024-04-30 19:58:44 +0800

    Added admin folder and admin-css folder for content dashboard, logs, edit-event, admins, and add-event.

---
+++ admin/admins.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="../admin-css/admins.css">
+<link href="../images/logo.png" rel="icon">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Content Dashboard</title>
+</head>
+<body>
+    <header class="header">
+		<h1 class="logo"><a href="../home.php"><img src="../images/logo-with-text.png" alt="barrio-liwanag-logo-with-text" width="200" height="75"></a></h1>
+            <ul class="main-nav">
+                <li><a href="content_dashboard.php">CONTENT DASHBOARD</a></li>
+                <li><a href="logs.php">LOGS</a></li>
+                <li><a href="admins.php">ADMINS</a></li>
+                <li><a href="logout.php">LOGOUT</a></li>
+            </ul>
+	</header>
+    <footer>
+        <center>
+        <p>&copy; Copyright Barrio Liwanag. All Rights Reserved 2024</p>
+        </center>
+    </footer>
+</body>
+</html>
